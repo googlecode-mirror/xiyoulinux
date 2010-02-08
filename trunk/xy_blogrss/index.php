3cbe8cee0169a4b990e1e4ba78b78ec686dd3d69
Helight.Xu <Helight.Xu@b21f8542-f22f-11de-b359-dd99991af1b2>
2010-02-08 04:39:13 +0000

    add xiyou blog rss system

---
+++ trunk/xy_blogrss/index.php
@@ -0,0 +1,31 @@
+<?php
+	include "./includes/func.php";
+	include "./includes/mysql_op.php";
+//	include "./includes/xml_op.php";
+	$feed_url=$_GET['feed_url'];
+?>
+<!-- begin of RSS show -->
+<?php
+	show_top();
+?>
+<body>
+<div id="page">
+ <?php require('./includes/head.php');?>
+<div id="content">
+	<div id="sub_list">		<!-- begin of list show -->
+<?php
+	show_list();
+?>
+	<div id="space"></div>
+	</div>				<!-- end of list show -->
+	<div id="blog">			<!-- begin of contentWrapper -->
+<?php
+	ShowOneRSS($feed_url);
+?>
+	</div>				<!-- end of contentWrapper -->
+</div>					<!-- end of mainwindow -->
+</div>
+<!-- begin of bottom show -->
+   <?php require("./includes/foot.php")?>
+</body>
+</html>
