023696ad85e6a91d032f153361414d6bce2badae
skxiaonan <skxiaonan@b21f8542-f22f-11de-b359-dd99991af1b2>
2010-02-09 05:38:54 +0000

    update api

--- trunk/xy_project_management/code/project_info_management/project_config.php
+++ trunk/xy_project_management/code/project_info_management/project_config.php
@@ -2,4 +2,5 @@
 	define('project_table_name', 'xy_project');
 	define('project_info_path', dirname(__FILE__));
 	define('project_images_path', '');
+	define('project_feeds_google', 'http://code.google.com/feeds');
 ?>
